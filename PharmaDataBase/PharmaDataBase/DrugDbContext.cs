b62d7d70e071fd75f52c0df79741a0c26431f40b
Al-Shahali <61845394+Al-Shahali@users.noreply.github.com>
2025-07-24 12:23:10 +0300

    Update DrugDbContext.cs
    Remove connection

--- PharmaDataBase/PharmaDataBase/DrugDbContext.cs
+++ PharmaDataBase/PharmaDataBase/DrugDbContext.cs
@@ -4,7 +4,7 @@ namespace PharmaDataBase
 {
     public class DrugDbContext : DbContext
     {
-        string _connectionString = "Server=db24045.public.databaseasp.net; Database=db24045; User Id=db24045; Password=D@a6qE9_L+c7; Encrypt=True; TrustServerCertificate=True;";
+        string _connectionString = "";
         public DbSet<Drug> Drugs { get; set; }
         public DbSet<DrugResponse> DrugResponses { get; set; }
 
